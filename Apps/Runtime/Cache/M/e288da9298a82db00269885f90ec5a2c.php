e0b4b41a17da4eb0e1ca43642eeed39308e4064c
ritacc <ritacc@126.com>
2015-12-23 00:10:52 +0800

    Signed-off-by: ritacc <ritacc@126.com>

---
+++ Apps/Runtime/Cache/M/e288da9298a82db00269885f90ec5a2c.php
@@ -0,0 +1,12 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title></title>
+	</head>
+	<body>
+		token: 	<span><?php echo ($token); ?></span><br/>
+		
+		
+	</body>
+</html>

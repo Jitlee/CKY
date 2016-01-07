593fcf9141cd0117a9f8d9eccbc798be59ea1fab
Jitlee <www.wpj@163.com>
2016-01-07 09:34:03 +0800

    添加折扣信息

--- Apps/M/Model/ShopsModel.class.php
+++ Apps/M/Model/ShopsModel.class.php
@@ -16,9 +16,8 @@ class ShopsModel extends BaseModel {
      public function shops(){
      	$pageSize = 10;
 		$pageNo = intval(I('pageNo', 1));
-	 	$sql = sprintf('select s.shopId,shopSn,shopName,shopImg,shopTel,shopAtive,shopStatus,latitude,longitude,IFNULL(c.shopKeywords, gc.catName) shopKeywords
+	 	$sql = sprintf('select s.shopId,shopSn,shopName,shopImg,shopTel,shopAtive,shopStatus,latitude,longitude,deliveryOff,shopAddress,gc.catName
 	 		from __PREFIX__shops s join __PREFIX__goods_cats gc on gc.catId = s.goodsCatId1
-	 		left join __PREFIX__shop_configs c on s.shopId = c.shopId
 	 	    where shopStatus=1 and shopFlag=1 order by shopId desc limit %d, %d', ($pageNo - 1) * $pageSize, $pageSize);
 		return $this->query($sql);
 	 }

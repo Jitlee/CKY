3bcd775eff50aea80ffff0f1bf7beee9e151f736
Jitlee <www.wpj@163.com>
2016-02-25 00:40:55 +0800

    添加券的样式

--- Apps/M/Model/ActivityTicketModel.class.php
+++ Apps/M/Model/ActivityTicketModel.class.php
@@ -12,10 +12,10 @@ class ActivityTicketModel extends BaseModel {
     public function queryAll($uid) {
     		$pageSize = 12;
 		$pageNo = intval(I('pageNo', 1));
-    		return $this->field('s.shopImg, s.shopName, t.limitUserShopId, t.ticketID, t.title, t.imagePath, t.ticketAmount, t.totalCount, t.userCount, t.efficacySDate, t.efficacyEDate, t.miniConsumption, t.typeName, t.content,  isnull(tm.uid) isReceived')
-			->join('t inner join __SHOPS__ s on s.shopId = t.limitUseShopID')
-    			->join('left join __TICKET_M__ tm on t.ticketID = tm.ticketID')
-			->where('t.ticketStatus = 1 and t.efficacyEDate < now()')
+    		return $this->field('s.shopImg, s.shopName, t.limitUseShopId, t.ticketID, t.title, t.imagePath, t.ticketAmount, t.totalCount, t.usedCount, t.efficacySDate, t.efficacyEDate, t.miniConsumption, t.typeName, t.content,  isnull(tm.uid) isReceived')
+			->join('t left join __SHOPS__ s on s.shopId = t.limitUseShopID')
+    			->join('left join __ACTIVITY_TICKET_M__ tm on t.ticketID = tm.ticketID')
+			->where('t.ticketStatus = 1 and t.efficacyEDate >= CURDATE()')
     			->order('t.createTime')->page($pageNo, $pageSize)->select();
     }
 };

3751c53e6a99c1dca4a60d5a18d509ee0bd58c6a
Jitlee <www.wpj@163.com>
2016-02-26 22:00:33 +0800

    修复 查询券列表

--- Apps/M/Model/ActivityTicketModel.class.php
+++ Apps/M/Model/ActivityTicketModel.class.php
@@ -14,7 +14,7 @@ class ActivityTicketModel extends BaseModel {
 		$pageNo = intval(I('pageNo', 1));
     		return $this->field('s.shopImg, s.shopName, t.limitUseShopId, t.ticketID, t.title, t.imagePath, t.ticketAmount, t.totalCount, t.usedCount, t.efficacySDate, t.efficacyEDate, t.miniConsumption, t.typeName, t.content,  isnull(tm.uid) isReceived')
 			->join('t left join __SHOPS__ s on s.shopId = t.limitUseShopID')
-    			->join('left join __ACTIVITY_TICKET_M__ tm on t.ticketID = tm.ticketID')
+    			->join('left join __ACTIVITY_TICKET_M__ tm on t.ticketID = tm.ticketID and tm.uid = '.$uid.'')
 			->where('t.ticketStatus = 1 and t.efficacyEDate >= CURDATE()')
     			->order('t.createTime')->page($pageNo, $pageSize)->select();
     }

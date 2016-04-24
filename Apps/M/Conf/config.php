7cbc129edecea68edbff6d0598cfdb43a41c7b9f
Jitlee <www.wpj@163.com>
2016-04-24 22:38:09 +0800

    修改秒杀排序

--- Apps/M/Conf/config.php
+++ Apps/M/Conf/config.php
@@ -8,7 +8,7 @@
 			'__JQ__'				=> '//cdn.bootcss.com/jquery/1.11.3/jquery.min.js',
 			'__KO__'				=> '/Apps/M/View/default/js/knockout-3.4.0.js',
 
-			'__FONT__'				=> '//at.alicdn.com/t/font_1461336644_495359.css',
+			'__FONT__'				=> '//at.alicdn.com/t/font_1461502836_5137036.css',
 			'__IMG__'				=> '/Apps/M/View/default/images',
 
 			'__JS__'				=> '/Apps/M/View/default/js',

264392040a95a4eab9acf5f90f4a0eb296a1af5d
Jitlee <www.wpj@163.com>
2016-04-26 00:19:17 +0800

    商城首页界面第一次提交

--- Apps/M/Action/MallAction.class.php
+++ Apps/M/Action/MallAction.class.php
@@ -11,11 +11,19 @@ namespace M\Action;
 use Think\Controller;
 class MallAction extends BaseAction {
 	public function index() {
-		
 		// 获取广告
 		$addb = D('ads');
 		$ads = $addb->queryByType(-4);
 		$this->assign('ads', $ads);
+		
+		// 获取一元购顶端广告
+		$ads = $addb->queryByType(-5);
+		$this->assign('yygAds', $ads[0]);
+		
+		// 促销活动
+		$madb = D('MallActivity');
+		$activities = $madb->queryTop6();
+		$this->assign('activites', $activities);
 		
 		$this->assign('title', '商城');
 		$this->display();

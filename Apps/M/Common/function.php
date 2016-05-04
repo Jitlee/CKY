57baa0a4c68974319a6b6e0734be4d6fe79a6fee
ritacc <ritacc@126.com>
2016-05-05 00:10:34 +0800

    Signed-off-by: ritacc <ritacc@126.com>

--- Apps/M/Common/function.php
+++ Apps/M/Common/function.php
@@ -133,38 +133,35 @@ function getScore($i=5)
 }
 
 function test_login() {
-	$openid=session('openid').'';
-	// 测试默认用户 
-//	if(strlen($openid)<10) {
-//		$openid="o4CBRwu4gN7w8JZsVCw6leu9g2-Y";
-//		session('openid',$openid);
-//	}
-	
-	//如果openid不存在重新获取
-	if(strlen($openid) <= 10) {
-		$openid = get_user_open_id();
-	}
-	//echo $openid;
-	if(strlen($openid) > 10) {
-		$result=session("MemberItem");
-		$strcardid=$result["CardId"];
-		if(strlen($strcardid) <= 3) {//session 中不存在。
-			//验证是否已经关联
-			$mMember = D('M/Member');
-			$result = $mMember->GetByOpenid($openid);
-			if(!$result) { //如果用户不存在
-				$this->redirect('Home/selectreg');
+		$openid=session('openid').'';	
+		//如果openid不存在重新获取
+		if(strlen($openid) <= 10) {
+			$openid = get_user_open_id();
+		}
+		//echo $openid;
+		if(strlen($openid) > 10) {
+			$result=session("MemberItem");
+			$strcardid=$result["CardId"];
+			if(strlen($strcardid) <= 3) {//session 中不存在。
+				//验证是否已经关联
+				$mMember = D('M/Member');
+				$result = $mMember->GetByOpenid($openid);
+				if(!$result) { //如果用户不存在					
+					$REDIRECT_URI='http://' . $_SERVER['HTTP_HOST'] . U('Home/selectreg', '', '');
+					header('Location:'.$REDIRECT_URI);	
+				}
 			}
+			if($result) {
+				session("cardid",$result["CardId"]);					
+				session("uid",$result["uid"]);
+				session("Mobile",$result["Mobile"]);
+				session("MemberItem",$result);	
+			}
+		} else {
+			$REDIRECT_URI='http://' . $_SERVER['HTTP_HOST'] . U('Home/getwxerror', '', '');
+			header('Location:'.$REDIRECT_URI);
+			
 		}
-		if($result) {
-			session("cardid",$result["CardId"]);					
-			session("uid",$result["uid"]);
-			session("Mobile",$result["Mobile"]);
-			session("MemberItem",$result);	
-		}
-	} else {
-		$this->redirect('Home/getwxerror');
-	}
 }
 /*
  * 尝试登录 不用强制跳转
